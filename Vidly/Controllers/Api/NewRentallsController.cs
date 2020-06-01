f5ce037a3db8010fa0bdcd50099569342022cbf5
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2020-06-01 15:55:11 +0200

    Adding the details

--- Vidly/Controllers/Api/NewRentallsController.cs
+++ Vidly/Controllers/Api/NewRentallsController.cs
@@ -5,15 +5,34 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentallsController : ApiController
     {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MoviesIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            return Ok();
         }
     }
 }
